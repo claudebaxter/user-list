64d21950c2460a876bba0e564ce89a1caebc5b59
claudebaxter <e.baxter@dark-coin.io>
2023-07-29 14:29:42 -0500

    Removing console.log tests.

--- src/App.tsx
+++ src/App.tsx
@@ -34,10 +34,6 @@ export default function App() {
     fetchUsersData();
   }, []);
 
-  useEffect(() => {
-    console.log('Selected user:', selectedUser);
-  }, [selectedUser]);
-
   function formatUser(user: User): FormattedUser {
     const friendNames = user.friends.map((friendId) => {
       const friend = users.find((u) => u.id === friendId);
@@ -59,7 +55,6 @@ export default function App() {
   
   function handleUserClick(userId: string) {
     setSelectedUser(userId);
-    console.log('User clicked:', userId);
   }
 
   function filterUsers(user: User): boolean {
